e972ed9f4a1fca3ae06af07f94dfd1b95cf8d5ad
Ottermandias <nickroederspam+git1@gmail.com>
2023-07-15 15:01:21 +0200

    Fix unavailable customizations set to 0 failing validity.

--- Glamourer/Services/CustomizationService.cs
+++ Glamourer/Services/CustomizationService.cs
@@ -122,7 +122,7 @@ public sealed class CustomizationService : AsyncServiceWrapper<ICustomizationMan
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static bool IsCustomizationValid(CustomizationSet set, CustomizeValue face, CustomizeIndex type, CustomizeValue value,
         [NotNullWhen(true)] out CustomizeData? data)
-        => set.DataByValue(type, value, out data, face) >= 0;
+        => set.DataByValue(type, value, out data, face) >= 0 || !set.IsAvailable(type) && value.Value == 0;
 
     /// <summary> Returns whether a customization value is valid for a given clan, gender and face. </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
