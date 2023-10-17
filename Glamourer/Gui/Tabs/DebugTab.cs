8b8b14f2b60dc1781d9114605600a8e01c4600cf
Ottermandias <nickroederspam+git1@gmail.com>
2023-10-17 19:51:17 +0200

    Show and update BNPCs.

--- Glamourer/Gui/Tabs/DebugTab.cs
+++ Glamourer/Gui/Tabs/DebugTab.cs
@@ -730,12 +730,47 @@ public unsafe class DebugTab : ITab
 
         disabled.Dispose();
 
-        DrawNameTable("BNPCs",      ref _bnpcFilter,      _actors.AwaitedService.Data.BNpcs.Select(kvp => (kvp.Key, kvp.Value)));
+        DrawBnpcTable();
         DrawNameTable("ENPCs",      ref _enpcFilter,      _actors.AwaitedService.Data.ENpcs.Select(kvp => (kvp.Key, kvp.Value)));
         DrawNameTable("Companions", ref _companionFilter, _actors.AwaitedService.Data.Companions.Select(kvp => (kvp.Key, kvp.Value)));
         DrawNameTable("Mounts",     ref _mountFilter,     _actors.AwaitedService.Data.Mounts.Select(kvp => (kvp.Key, kvp.Value)));
         DrawNameTable("Ornaments",  ref _ornamentFilter,  _actors.AwaitedService.Data.Ornaments.Select(kvp => (kvp.Key, kvp.Value)));
         DrawNameTable("Worlds",     ref _worldFilter,     _actors.AwaitedService.Data.Worlds.Select(kvp => ((uint)kvp.Key, kvp.Value)));
+    }
+
+    private void DrawBnpcTable()
+    {
+        using var _    = ImRaii.PushId(1);
+        using var tree = ImRaii.TreeNode("BNPCs");
+        if (!tree)
+            return;
+
+        var resetScroll = ImGui.InputTextWithHint("##filter", "Filter...", ref _bnpcFilter, 256);
+        var height      = ImGui.GetTextLineHeightWithSpacing() + 2 * ImGui.GetStyle().CellPadding.Y;
+        using var table = ImRaii.Table("##table", 3, ImGuiTableFlags.RowBg | ImGuiTableFlags.ScrollY | ImGuiTableFlags.BordersOuter,
+            new Vector2(-1, 10 * height));
+        if (!table)
+            return;
+
+        if (resetScroll)
+            ImGui.SetScrollY(0);
+        ImGui.TableSetupColumn("1", ImGuiTableColumnFlags.WidthFixed, 50 * ImGuiHelpers.GlobalScale);
+        ImGui.TableSetupColumn("2", ImGuiTableColumnFlags.WidthStretch);
+        ImGui.TableSetupColumn("3", ImGuiTableColumnFlags.WidthStretch);
+        ImGui.TableNextColumn();
+        var skips = ImGuiClip.GetNecessarySkips(height);
+        ImGui.TableNextRow();
+        var data = _actors.AwaitedService.Data.BNpcs.Select(kvp => (kvp.Key, kvp.Key.ToString("D5"), kvp.Value));
+        var remainder = ImGuiClip.FilteredClippedDraw(data, skips,
+            p => p.Item2.Contains(_bnpcFilter) || p.Item3.Contains(_bnpcFilter, StringComparison.OrdinalIgnoreCase),
+            p =>
+            {
+                ImGuiUtil.DrawTableColumn(p.Item2);
+                ImGuiUtil.DrawTableColumn(p.Item3);
+                var bnpcs = _items.IdentifierService.AwaitedService.GetBnpcsFromName(p.Item1);
+                ImGuiUtil.DrawTableColumn(string.Join(", ", bnpcs.Select(b => b.Id.ToString())));
+            });
+        ImGuiClip.DrawEndDummy(remainder, height);
     }
 
     private static void DrawNameTable(string label, ref string filter, IEnumerable<(uint, string)> names)
