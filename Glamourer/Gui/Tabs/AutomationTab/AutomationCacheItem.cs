479ecaae004a0da3b8a8407b1ef691739ac5c82c
Ottermandias <nickroederspam+git1@gmail.com>
2026-04-02 16:29:51 +0200

    Fix actor identifier display.

--- Glamourer/Gui/Tabs/AutomationTab/AutomationCacheItem.cs
+++ Glamourer/Gui/Tabs/AutomationTab/AutomationCacheItem.cs
@@ -1,5 +1,8 @@
-﻿using Glamourer.Automation;
+﻿using Dalamud.Game.ClientState.Objects.Enums;
+using Glamourer.Automation;
 using ImSharp;
+using Penumbra.GameData.Actors;
+using Penumbra.GameData.Enums;
 
 namespace Glamourer.Gui.Tabs.AutomationTab;
 
@@ -8,7 +11,23 @@ public readonly struct AutomationCacheItem(AutoDesignSet set, int index)
     public readonly AutoDesignSet Set                 = set;
     public readonly int           Index               = index;
     public readonly StringPair    Name                = new(set.Name);
-    public readonly StringPair    IdentifierString    = new(set.Identifiers.First().ToString());
+    public readonly StringPair    IdentifierString    = GetIdentifierPair(set.Identifiers.First());
     public readonly StringU8      Incognito           = new($"Auto Design Set #{index + 1}");
-    public readonly StringU8      IdentifierIncognito = new(set.Identifiers.First().Incognito(null));
+    public readonly StringU8      IdentifierIncognito = GetIncognito(set.Identifiers.First());
+
+    private static StringPair GetIdentifierPair(ActorIdentifier identifier)
+        => identifier switch
+        {
+            { Type: IdentifierType.Npc, Kind: ObjectKind.BattleNpc } => new StringPair($"{identifier.ToName()} (BNPC)"),
+            { Type: IdentifierType.Npc, Kind: ObjectKind.EventNpc }  => new StringPair($"{identifier.ToName()} (ENPC)"),
+            _                                                        => new StringPair(identifier.ToString()),
+        };
+
+    private static StringU8 GetIncognito(ActorIdentifier identifier)
+        => identifier switch
+        {
+            { Type: IdentifierType.Npc, Kind: ObjectKind.BattleNpc } => new StringU8($"{identifier.ToName()} (BNPC)"),
+            { Type: IdentifierType.Npc, Kind: ObjectKind.EventNpc }  => new StringU8($"{identifier.ToName()} (ENPC)"),
+            _                                                        => new StringU8(identifier.Incognito(null)),
+        };
 }
