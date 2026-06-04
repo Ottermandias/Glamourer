f91bc868f9a29d2f0b92bffcc9a085bb074f8c95
Exter-N <exter-n@exter-n.fr>
2026-06-04 03:56:35 +0200

    ClientStructs stuff

--- Glamourer/Interop/Material/MaterialValueIndex.cs
+++ Glamourer/Interop/Material/MaterialValueIndex.cs
@@ -107,7 +107,7 @@ public readonly record struct MaterialValueIndex(
     }
 
     public unsafe bool TryGetTextures(Actor actor, out ReadOnlySpan<FFXIVClientStructs.Interop.Pointer<Texture>> textures,
-        out ReadOnlySpan<FFXIVClientStructs.Interop.Pointer<CsMaterial>> materials)
+        out ReadOnlySpan<FFXIVClientStructs.Interop.Pointer<MaterialResourceHandle>> materials)
     {
         if (!TryGetModel(actor, out var model)
          || SlotIndex >= model.AsCharacterBase->SlotCount
@@ -160,12 +160,12 @@ public readonly record struct MaterialValueIndex(
     }
 
     public unsafe bool TryGetTexture(ReadOnlySpan<FFXIVClientStructs.Interop.Pointer<Texture>> textures,
-        ReadOnlySpan<FFXIVClientStructs.Interop.Pointer<CsMaterial>> materials,
+        ReadOnlySpan<FFXIVClientStructs.Interop.Pointer<MaterialResourceHandle>> materials,
         out Texture** texture, out ColorRow.Mode mode)
     {
         mode = MaterialIndex >= materials.Length
             ? ColorRow.Mode.Dawntrail
-            : PrepareColorSet.GetMode((MaterialResourceHandle*)materials[MaterialIndex].Value);
+            : PrepareColorSet.GetMode(materials[MaterialIndex].Value);
 
 
         if (MaterialIndex >= textures.Length || textures[MaterialIndex].Value == null)
